224bb36e384d64ef3e04ffb48bca836cb49b1b29
DeicPro <DeicPro@users.noreply.github.com>
2015-06-07 21:29:31 +0200

    Update Your-Script.sh

--- Your-Script.sh
+++ Your-Script.sh
@@ -4,30 +4,33 @@
 #Variables
 #Version of your script = 1.0_stable
 var(){
-name="SH-OTA.sh"
-download="https://www.Your-Site/SH-OTA.sh"
-check="$EXTERNAL_STORAGE/Download/$name"
+#Edit this:
+	name="SH-OTA.sh"
+	download="https://www.Your-Site/SH-OTA.sh"
+#Don't edit:
+	check="$EXTERNAL_STORAGE/Download/$name"
+	browser="am start -a android.intent.action.VIEW -n com.android.browser/.BrowserActivity"
+	term="am start -a android.intent.action.MAIN -n jackpal.androidterm/.Term"
+	null=">/dev/null 2>&1"
 }
 
 download(){
-am start -a android.intent.action.VIEW -n com.android.browser/.BrowserActivity $download >/dev/null 2>&1
-clear
-am start -a android.intent.action.MAIN -n jackpal.androidterm/.Term >/dev/null 2>&1
-clear
-check_update
+	$browser $download $null
+	$term $null
+	check_update
 }
 
 check_update(){
-if [ -e $check ]; then
-am force-stop com.android.browser
-$SHELL -c $check
-else
-wait_download
-fi
+	if [ -e $check ]; then
+		am force-stop com.android.browser
+		$SHELL -c $check
+	else
+		wait_download
+	fi
 }
 
 wait_download(){
-check_update
+	check_update
 }
 
 #Start
