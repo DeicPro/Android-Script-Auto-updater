6ef71702e01a59492b4ec62a4806120c5d39ba0d
Pablo <DeicPro@users.noreply.github.com>
2015-06-14 07:04:39 +0200

    Update SH-OTA.sh

--- SH-OTA.sh
+++ SH-OTA.sh
@@ -1,105 +1,97 @@
 #SH-OTA By Deic & DiamondBond
-
 #Variables
-a1(){
 #From here edit
-	na="Your-Script.sh" #Name of your script file
-	ve="1.0_stable" #Version of your script
-	so="/system/xbin/" #Location of your script
-	do="https://www.Your-Site.com/Your-Script.sh" #Link of your script
-	ns="SH-OTA.sh" #Name of your SH-OTA file
+n="Your-Script.sh" #Name of your script file
+v="1.0_stable" #Version of your script
+s="/system/xbin/" #Location of your script
+d="https://www.Your-Site.com/Your-Script.sh" #Link of your script
+h="SH-OTA.sh" #Name of your SH-OTA file
 #From here don't edit
-	ch="$EXTERNAL_STORAGE/Download/$ns"
-	sc="$EXTERNAL_STORAGE/Download/$na"
-	br="com.android.browser"
-	ec="echo"
-	cl="clear"
-	rm=`rm -rf`
-	sl=`sleep 1`
-	mo=`mount -o remount`
-}
-
+k=" $EXTERNAL_STORAGE/Download/$h"
+i=" $EXTERNAL_STORAGE/Download/$n "
+b=" com.android.browser "
+e="echo "
+c="clear"
+r=`rm -f`
+s=`sleep 1`
+m=`mount -o remount`
+a=`am start`
 #Safe exit
-a2(){
-	$rm $ch
-	$rm $sc
-	$mo ro /system
-	$cl
-	$SHELL -c $so$na
-	exit
+2(){
+$r$k
+$r$i
+$m,ro /system
+$c
+$SHELL -c $s/$n
+exit
 }
-
 #Check update
-a3(){
-	$ec
-	$ec "Checking updates..."
-	$sl
-	if [ grep $ve $so/$na >/dev/null 2>&1 ]; then
-		$cl
-		$rm $ch
-		$ec
-		$ec "You have the latest version."
-		$sl
-		a2
-	else
-		a4
-	fi
+3(){
+$e
+$e"Checking updates..."
+$s
+if["`grep $v $s/$n 1>/dev/null`"]
+then
+$c
+$r$k
+$e
+$e"You have the latest version."
+$s
+2
+else
+4
+fi
 }
-
 #Ask download
-a4(){
-	$cl
-	$ec
-	$ec "A new version of the script was found..."
-	$ec
-	$ec "Want download it? (Y/N)"
-	$ec
-	$ec -ne "> "
-	read op
-	case $op in
-		y|Y) a5;;
-		n|N) a2;;
-		*) $ec "Write Y or N and press enter..." && a4;;
-	esac
+4(){
+$c
+$e
+$e"A new version of the script was found..."
+$e
+$e"Want download it? (Y/N)"
+$e
+$e-ne"> "
+read op
+case $op in
+y|Y)5;;
+n|N)2;;
+*)$e"Write Y/N and press enter..."&&4;;
+esac
 }
-
 #Download update
-a5(){
-	$cl
-	$ec
-	$ec "Downloading..."
-	$sl
-	am start android.intent.action.VIEW $br $do >/dev/null 2>&1
-	am start jackpal.androidterm >/dev/null 2>&1
-	$cl
-	a6
+5(){
+$c
+$e
+$e"Downloading..."
+$s
+$a android.intent.action.VIEW$b$d 1>/dev/null
+$a jackpal.androidterm 1>/dev/null
+$c
+6
 }
-
 #Wait download
-a6(){
-	a7
+6(){
+	7
 }
-
 #Install update
-a7(){
-	if [ -e $sc ]; then
-		am force-stop $br
-		$ec
-		$ec "Installing..."
-		cp -f -r $sc $so
-		sleep 2
-		chmod 777 $so$na
-		$ec
-		$ec "Done."
-		$sl
-		a2
-	else
-		a6
-	fi
+7(){
+if[-e$i]
+then
+am force-stop$b
+$e
+$e"Installing..."
+cp -f$i$s
+sleep 2
+chmod 755 $s/$n
+$e
+$e"Done."
+$s
+2
+else
+6
+fi
 }
-
 #Script start
-$cl
-a1
-$mo rw /system
-a3
+$c
+$m,rw /system
+3
