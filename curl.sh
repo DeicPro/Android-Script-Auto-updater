3ea587066cd287ea6f7a44c5ed093e8d5c6c72d2
Pablo <DeicPro@users.noreply.github.com>
2015-06-15 16:33:06 +0200

    moar genius

--- curl.sh
+++ curl.sh
@@ -5,13 +5,27 @@ tmp="/data/local/tmp"
 xbin="/system/xbin"
 ssl="/data/local/ssl"
 curl_cloud="https://github.com/DeicPro/curl/releases/download/curl"
-start_browser=`am start com.android.browser`
-stop_browser`am force-stop com.android.browser
+start_browser=`am start android.intent.actions.VIEW com.android.browser`
 
-$start_browser $tmp/curl $curl_cloud/curl
-$start_browser $tmp/openssl $curl_cloud/openssl
-$start_browser $tmp/openssl.cnf $curl_cloud/openssl.cnf
-$start_browser $tmp/ca-budle.crt $curl_cloud/ca-budle.crt
+echo
+echo "Downloading curl binary..."
+sleep 1
+
+$start_browser $tmp/curl $curl_cloud/curl 2>/dev/null
+echo
+echo "Downloading openssl binary..."
+sleep 1
+
+$start_browser $tmp/openssl $curl_cloud/openssl 2>/dev/null
+$start_browser $tmp/openssl.cnf $curl_cloud/openssl.cnf 2>/dev/null
+$start_browser $tmp/ca-budle.crt $curl_cloud/ca-budle.crt 2>/dev/null
+
+if [ -f ca-budle.crt ]
+echo
+echo "Installing..."
+sleep 1
+
+am force-stop com.android.browser
 cp -rf $tmp/curl $xbin/
 cp -rf $tmp/openssl $xbin/
 mkdir -p $ssl/
@@ -25,4 +39,9 @@ chmod 755 $ssl/certs/
 chmod 755 $ssl/openssl.cnf
 chmod 755 $ssl/certs/ca-budle.crt
 
+echo
+echo "Done."
+sleep 1
+
 exit
+fi
