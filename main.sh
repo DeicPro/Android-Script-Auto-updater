3ad222818fc34011a322ef6cb8610e60fa37ef04
Pablo <DeicPro@users.noreply.github.com>
2015-06-15 12:18:59 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,16 @@
 #SH-OTA v1.2_wip By Deic & DiamondBond
 
+script=/system/xbin/yourscript
+
+mount-o remount,rw /system
+mount-o remount,rw /data
 if [ ! -f /system/bin/curl ] || [ ! -f /system/xbin/curl ] || [ ! -f /sbin/curl ]
 then
 curl -k -L curlurl | sh
 else
 curl -k -L otaurl | sh
 fi
+mount-o remount,ro /system
+mount-o remount,ro /data
+$SHELL -c $script
+exit
