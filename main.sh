2c2c23c2b4bea35fbad931fd46807242d157962f
Pablo <DeicPro@users.noreply.github.com>
2015-06-15 16:05:52 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,16 +1,32 @@
-#SH-OTA v1.2_wip By Deic & DiamondBond
+#SH-OTA v1.2_alpha By Deic & DiamondBond
 
-script=/system/xbin/yourscript
+#Edit from here
+script="location of your script"
+ota_cloud="link to ota.sh"
+curl_cloud="link to curl.sh"
 
-mount-o remount,rw /system
-mount-o remount,rw /data
-if [ ! -f /system/bin/curl ] || [ ! -f /system/xbin/curl ] || [ ! -f /sbin/curl ]
+#Don't edit from here
+curl_ext="$EXTERNAL_STORAGE/Download/curl.sh"
+curl_tmp="/data/local/tmp/curl.sh"
+mount=`mount-o remount`
+
+$mount,rw /system
+$mount,rw /data
+
+if [ ! -f /system/bin/curl ] || [ ! -f /system/xbin/curl ]
 then
-curl -k -L curlurl | sh
+am start com.android.browser $curl_cloud
+elif [ -f $curl_ext ]
+then
+am force-stop com.android.browser
+cp -rf $curl_ext $curl_tmp
+chmod 755 $curl_tmp
+$SHELL -c $curl_tmp
 else
-curl -k -L otaurl | sh
+curl -k -L $ota_cloud | sh
 fi
-mount-o remount,ro /system
-mount-o remount,ro /data
+
+$mount,ro /system
+$mount,ro /data
 $SHELL -c $script
 exit
