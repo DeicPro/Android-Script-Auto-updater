a808abec2a922ba98ae4bc6f5ea79d835c927118
Pablo <DeicPro@users.noreply.github.com>
2015-06-15 11:35:41 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,8 @@
-wip
+#SH-OTA v1.2_wip By Deic & DiamondBond
+
+if [ ! -f /system/bin/curl ] || [ ! -f /system/xbin/curl ] || [ ! -f /sbin/curl ]
+then
+curl -k -L curlurl | sh
+else
+curl -k -L otaurl | sh
+fi
