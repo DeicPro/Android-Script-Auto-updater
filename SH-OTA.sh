cc20a2a0c2279eb3457426ef007aa43322a82944
Pablo <DeicPro@users.noreply.github.com>
2015-06-15 10:04:39 +0200

    Update SH-OTA.sh

--- SH-OTA.sh
+++ SH-OTA.sh
@@ -1,97 +1,83 @@
-#SH-OTA By Deic & DiamondBond
-#Variables
-#From here edit
-n="Your-Script.sh" #Name of your script file
-v="1.0_stable" #Version of your script
-s="/system/xbin/" #Location of your script
-d="https://www.Your-Site.com/Your-Script.sh" #Link of your script
-h="SH-OTA.sh" #Name of your SH-OTA file
+#SH-OTA v1.2_wip script
+ota_name="SH-OTA.sh" #Name of your SH-OTA file
+
 #From here don't edit
-k=" $EXTERNAL_STORAGE/Download/$h"
-i=" $EXTERNAL_STORAGE/Download/$n "
-b=" com.android.browser "
-e="echo "
-c="clear"
-r=`rm -f`
-s=`sleep 1`
-m=`mount -o remount`
-a=`am start`
-#Safe exit
-2(){
-$r$k
-$r$i
-$m,ro /system
-$c
-$SHELL -c $s/$n
-exit
-}
-#Check update
-3(){
-$e
-$e"Checking updates..."
-$s
-if["`grep $v $s/$n 1>/dev/null`"]
+ota="$EXTERNAL_STORAGE/Download/$ota_name"
+script="$EXTERNAL_STORAGE/Download/$name"
+ec="echo "
+cl="clear"
+rm=`rm -f`
+sl=`sleep 1`
+mo=`mount -o remount`
+
+check_update(){
+$ec
+$ec "Checking updates..."
+$sl
+if [ "`grep $ver $loc/$name 1>/dev/null`" ]
 then
-$c
-$r$k
-$e
-$e"You have the latest version."
-$s
-2
+$cl
+$rm $ota
+$ec
+$ec "You have the latest version."
+$sl
+safe_exit
 else
-4
+ask_download
 fi
 }
-#Ask download
-4(){
-$c
-$e
-$e"A new version of the script was found..."
-$e
-$e"Want download it? (Y/N)"
+
+ask_download(){
+$cl
+$ec
+$ec "A new version of the script was found..."
+$ec
+$ec "Want download it? (Y/N)"
 $e
 $e-ne"> "
-read op
-case $op in
-y|Y)5;;
-n|N)2;;
-*)$e"Write Y/N and press enter..."&&4;;
+read opt
+case $opt in
+y|Y ) download_update;;
+n|N ) safe_exit;;
+* ) $ec "Write [Y] or [N] and press enter..."; ask_download;;
 esac
 }
-#Download update
-5(){
-$c
-$e
-$e"Downloading..."
-$s
-$a android.intent.action.VIEW$b$d 1>/dev/null
-$a jackpal.androidterm 1>/dev/null
-$c
-6
+
+download_update(){
+$cl
+$ec
+$ec "Downloading..."
+$sl
+curl -k -L $cloud | sh 1>/dev/null
+$cl
+install_update
 }
-#Wait download
-6(){
-	7
-}
-#Install update
-7(){
-if[-e$i]
+
+install_update(){
+if [ -e $script ]
 then
-am force-stop$b
-$e
-$e"Installing..."
-cp -f$i$s
+$ec
+$ec "Installing..."
+cp -f $script $loc
 sleep 2
-chmod 755 $s/$n
-$e
-$e"Done."
-$s
-2
-else
-6
+chmod 755 $loc/$name
+$ec
+$ec "Done."
+$sl
+safe_exit
 fi
 }
-#Script start
-$c
-$m,rw /system
-3
+
+safe_exit(){
+$rm $ota
+$rm $script
+$mo,ro /system
+$cl
+$SHELL -c $loc/$name
+exit
+}
+
+#Start
+$cl
+$mo,rw /system
+check_update
