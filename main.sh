0ad432595c39ef19b37ca3fb72b5f10864c744c5
Pablo <DeicPro@users.noreply.github.com>
2015-06-18 08:46:12 +0200

    hot fix

--- main.sh
+++ main.sh
@@ -1,42 +1,52 @@
 #SH-OTA v1.2_alpha By Deic & DiamondBond
 
 #Edit from here
-script="location of your script"
-ota_cloud="link to ota.sh"
-curl_cloud="link to curl.sh"
+script="location/script"
+ota_cloud="https://yoursite/ota.sh"
+curl_cloud="https://yoursite/curl.sh"
 
 #Don't edit from here
+tmp="/data/local/tmp"
 curl_ext="$EXTERNAL_STORAGE/Download/curl.sh"
-curl_tmp="/data/local/tmp/curl.sh"
-mount=`mount-o remount`
 
-clear
-$mount,rw /system
-$mount,rw /data
-
+run_ota(){
+#clear
 if [ -f /system/bin/curl ] || [ -f /system/xbin/curl ]; then
-curl -k -L $ota_cloud | sh >/dev/null 2>&1
+curl -k -L -o $tmp/ota.sh $ota_cloud #>/dev/null 2>&1
+sleep 2
+chmod 755 $tmp/ota.sh
+$SHELL -c $tmp/ota.sh
+safe_exit
 else
 echo
 echo "curl & openssl binaries don't found."
-am start -a com.android.browser/.BrowserActivity -n $curl_cloud >/dev/null 2>&1
-install
-fi
-
-install(){
-clear
-if [ -f $curl_ext ]; then
-am force-stop com.android.browser
-cp -rf $curl_ext $curl_tmp
-chmod 755 $curl_tmp
-$SHELL -c $curl_tmp
-else
-install
+am start -a android.intent.action.VIEW -n com.android.browser/.BrowserActivity $curl_cloud #>/dev/null 2>&1
+install_curl
 fi
 }
 
-$mount,ro /system
-$mount,ro /data
-$SHELL -c $script
+install_curl(){
+#clear
+if [ -f $curl_ext ]; then
+am force-stop com.android.browser
+cp -rf $curl_ext $tmp/curl.sh
+rm -rf $curl_ext
+chmod 755 $tmp/curl.sh
+$SHELL -c $tmp/curl.sh
+run_ota
+else
+install_curl
+fi
+}
 
+safe_exit(){
+rm -rf $tmp/main.sh
+mount -o remount,ro /system
+mount -o remount,ro /data
+$SHELL -c $script
 exit
+}
+
+mount -o remount,rw /system
+mount -o remount,rw /data
+run_ota
