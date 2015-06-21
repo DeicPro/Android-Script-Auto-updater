ec3d7368e8f67739c132a5b086e63a7f9338e4a7
Deic <DeicPro@users.noreply.github.com>
2015-06-21 11:55:16 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,29 +10,29 @@ tmp="/data/local/tmp"
 curl_ext="$EXTERNAL_STORAGE/Download/curl.sh"
 
 run_ota(){
-#clear
+clear
 if [ -f /system/bin/curl ] || [ -f /system/xbin/curl ]; then
-curl -k -L -o $tmp/ota.sh $ota_cloud #>/dev/null 2>&1
+curl -k -L -o $tmp/ota.sh $ota_cloud >/dev/null 2>&1
 sleep 2
 chmod 755 $tmp/ota.sh
-$SHELL -c $tmp/ota.sh
+$tmp/ota.sh
 safe_exit
 else
 echo
 echo "curl & openssl binaries don't found."
-am start -a android.intent.action.VIEW -n com.android.browser/.BrowserActivity $curl_cloud #>/dev/null 2>&1
+am start -a android.intent.action.VIEW -n com.android.browser/.BrowserActivity $curl_cloud >/dev/null 2>&1
 install_curl
 fi
 }
 
 install_curl(){
-#clear
+clear
 if [ -f $curl_ext ]; then
 am force-stop com.android.browser
-cp -rf $curl_ext $tmp/curl.sh
-rm -rf $curl_ext
+cp -f $curl_ext $tmp/curl.sh
+rm -f $curl_ext
 chmod 755 $tmp/curl.sh
-$SHELL -c $tmp/curl.sh
+$tmp/curl.sh
 run_ota
 else
 install_curl
@@ -40,7 +40,7 @@ fi
 }
 
 safe_exit(){
-rm -rf $tmp/main.sh
+rm -f $tmp/main.sh
 mount -o remount,ro /system
 mount -o remount,ro /data
 $SHELL -c $script
