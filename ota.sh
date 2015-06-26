ef618a3add05ad8b74659a02b85f30e482891637
Deic <DeicPro@users.noreply.github.com>
2015-06-26 05:45:48 +0200

    Update ota.sh

--- ota.sh
+++ ota.sh
@@ -7,21 +7,34 @@ location="/system/xbin"
 cloud="https://your_site.com/your_script.sh"
 
 #Don't edit
-if [ "`grep $version $location/$name 2>/dev/null`" ]; then
-clear; echo "You have the latest version."; sleep 1; exit
+if [ "`grep $version $location/$name 2>/dev/null`" ]
+then
+clear
+echo "You have the latest version."
+sleep 1
+exit
 fi
 
-while true; do
-clear; echo "A new version of the script was found..."; echo
-echo "Want install it? (Y/N)"; echo
+while true
+do
+clear
+echo "A new version of the script was found..."
+echo
+echo "Want install it? (Y/N)"
+echo
 echo -n "> "
-read install_opt; case $install_opt in
+read install_opt
+case $install_opt in
 y|Y ) break;;
 n|N ) exit;;
-* ) echo "Write [Y] or [N] and press enter..."; sleep 1;;
+* ) echo "Write [Y] or [N] and press enter..."
+sleep 1;;
 esac
 done
 
-clear; echo "Downloading..."; sleep 1
+clear
+echo "Downloading..."
+sleep 1
 
-curl -k -L -o /data/local/tmp/$name $cloud >/dev/null 2>&1; exit
+curl -kLo /data/local/tmp/$name $cloud >/dev/null 2>&1
+exit
