2c978a6945c816a032329494cb81a17242f5267b
Deic <DeicPro@users.noreply.github.com>
2015-06-26 04:17:10 +0200

    Update SH-OTA.sh

--- SH-OTA.sh
+++ SH-OTA.sh
@@ -1,13 +1,13 @@
 #SH-OTA v1.2_alpha By Deic
 
 sh-ota(){
-#Edit from here
+#Edit values
 name="your_script.sh"
 version="version"
 location="/system/xbin"
 cloud="https://your_site.com/ota.sh"
 
-#Don't edit from here
+#Don't edit
 ext="$EXTERNAL_STORAGE/Download"
 tmp="/data/local/tmp"; xbin="/system/xbin"; ssl="/data/local/ssl"
 curl_cloud="https://github.com/DeicPro/Download/releases/download/curl"
@@ -18,18 +18,17 @@ mount -o remount,rw /system; mount -o remount,rw /data
 clear
 if [ ! -f $xbin/curl ]; then
 echo "Curl binaries not found."; echo
-echo "Downloading curl binaries..."; sleep 1
+clear; echo "Downloading curl binaries..."; sleep 1
 
 $start_browser/curl.file >/dev/null 2>&1; $start_browser/openssl.file >/dev/null 2>&1
-$start_browser/openssl_cnf.file >/dev/null 2>&1; $start_browser/ca-bundle_crt.file >/dev/null 2>&1; install_curl
-else
-download
+$start_browser/openssl_cnf.file >/dev/null 2>&1; $start_browser/ca-bundle_crt.file >/dev/null 2>&1
+curl="1"
 if
 
-install_curl(){
+if [ "$curl" == 1 ]; then
 while true; do
 if [ -f $ext/curl.file ] && [ -f $ext/openssl.file ] && [ -f $ext/openssl_cnf.file ] && [ -f $ext/ca-bundle_crt.file ]; then
-echo; echo "Installing..."; sleep 1
+clear; echo; echo "Installing..."; sleep 1
 
 am force-stop com.android.browser; mkdir -p $ssl/; mkdir -p $ssl/certs/
 rm -f $ext/curl.file $xbin/curl; rm -f $ext/openssl.file $xbin/openssl
@@ -42,23 +41,20 @@ chmod 755 $xbin/openssl; chmod 755 $ssl/openssl.cnf; chmod 755 $ssl/certs/ca-bun
 echo; echo "Done.";sleep 1; download
 fi
 done
-}
+fi
 
-download(){
+if [ ! "$curl" ]
 clear; echo "Checking updates..."; sleep 1
 
 curl -k -L -o $tmp/ota.sh $cloud >/dev/null 2>&1
 
 while true; do
 if [ -f $tmp/ota.sh ]; then
-chmod 755 $tmp/ota.sh; $tmp/ota.sh; install
-else
-break
+chmod 755 $tmp/ota.sh; $tmp/ota.sh; break
 fi
 done
-}
+fi
 
-install(){
 while true; do
 if [ -f $tmp/$name ]; then
 echo; echo "Installing..."; sleep 1
