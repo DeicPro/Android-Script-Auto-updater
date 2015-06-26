04f6b276bfe262e094df2da3d47efc8e0ca571a2
Deic <DeicPro@users.noreply.github.com>
2015-06-26 10:08:24 +0200

    Update SH-OTA.sh

--- SH-OTA.sh
+++ SH-OTA.sh
@@ -1,26 +1,29 @@
-SH-OTA(){#v1.2_alpha By Deic
+#SH-OTA v1.2_alpha By Deic
+
 #Edit values
-name="your_script.sh"
-version="version"
-location="/system/xbin"
-cloud="https://your_site.com/ota.sh"
+s_name="your_script.sh"
+s_version="version"
+s_location="/system/xbin"
+s_cloud="https://your_site.com/ota.sh"
 
 #Don't edit
-ext="$EXTERNAL_STORAGE/Download/curl.zip"
-ssl="/data/local/ssl"
-xbin="/system/xbin"
-ota="/tmp/ota.sh"
-tmp="/tmp/$name"
-script="$location/$name"
-mount_rw="mount -o remount,rw"
+s_info=/tmp/SH-OTA.info
+s_ext="$EXTERNAL_STORAGE/Download/curl.zip"
+s_ssl="/data/local/ssl"
+s_xbin="/system/xbin"
+s_ota="/tmp/ota.sh"
+s_tmp="/tmp/$name"
+s_script="$location/$name"
+s_mount_rw="mount -o remount,rw"
 
-$mount_rw rootfs
-$mount_rw /system
-$mount_rw /data
+$s_mount_rw rootfs
+$s_mount_rw /system
+$s_mount_rw /data
 mkdir -p /tmp/
-chmod 755 /tmp/
+touch $s_info
+chmod -R 755 /tmp/
 
-if [ ! -f $xbin/curl ]
+if [ ! -f $s_xbin/curl ]
 then clear
 echo "Curl binaries not found."
 sleep 1
@@ -31,20 +34,20 @@ am start -a android.intent.action.VIEW -n com.android.browser/.BrowserActivity h
 curl="1"
 fi
 
-if [ "$curl" == 1 ]
+if [ "$s_curl" == 1 ]
 then while true
-do if [ -f $ext ]
+do if [ -f $s_ext ]
 then clear
 echo "Installing..."
 sleep 1
 am force-stop com.android.browser
-unzip -oq $ext -d /tmp/
-cp -f /tmp/xbin/ $xbin/
-cp -f /tmp/ssl/ $ssl/
-chmod -R 755 $ssl/
-chmod 755 $xbin/curl
-chmod 755 $xbin/openssl
-rm -f $ext
+unzip -oq $s_ext -d /tmp/
+cp -f /tmp/xbin/ $s_xbin/
+cp -f /tmp/ssl/ $s_ssl/
+chmod -R 755 $s_ssl/
+chmod 755 $s_xbin/curl
+chmod 755 $s_xbin/openssl
+rm -f $s_ext
 clear
 echo "Installed."
 sleep 1
@@ -56,33 +59,33 @@ fi
 clear
 echo "Checking updates..."
 sleep 1
-curl -klos $ota $cloud
+curl -klos $s_ota $s_cloud
 
 while true
-do if [ -f $ota ]
-then chmod 755 $ota.sh
-$ota.sh
+do if [ -f $s_ota ]
+then chmod 755 $s_ota.sh
+$s_ota.sh
 break
 fi
 done
 
 then while true
-do if [ "`grep no /tmp/SH-OTA.info`" ]
+do if [ "`grep no $s_info`" ]
 then break
 fi
 
-if [ "`grep yes /tmp/SH-OTA.info`" ]
-then if [ -f $tmp ]
+if [ "`grep yes $s_info`" ]
+then if [ -f $s_tmp ]
 then clear
 echo "Installing..."
 sleep 1
-cp -f $tmp $script
-rm -f $tmp
-chmod 755 $script
+cp -f $s_tmp $s_script
+rm -f $s_tmp
+chmod 755 $s_script
 clear
 echo "Installed."
 sleep 1
-$script
+$s_script
 clear
 exit
 fi
