3bff8392eacb912a327314fc3ad3f159fe7a3459
Deic <DeicPro@users.noreply.github.com>
2015-06-26 11:38:34 +0200

    Update SH-OTA.sh

--- SH-OTA.sh
+++ SH-OTA.sh
@@ -7,21 +7,22 @@ s_location="/system/xbin"
 s_cloud="https://your_site.com/ota.sh"
 
 #Don't edit
+s_mount_rw="mount -o remount,rw"
 s_info=/tmp/SH-OTA.info
 s_ext="$EXTERNAL_STORAGE/Download/curl.zip"
 s_ssl="/data/local/ssl"
+s_certs="$s_ssl/certs/"
 s_xbin="/system/xbin"
 s_ota="/tmp/ota.sh"
-s_tmp="/tmp/$name"
-s_script="$location/$name"
-s_mount_rw="mount -o remount,rw"
+s_tmp="/tmp/$s_name"
+s_script="$s_location/$s_name"
 
 $s_mount_rw rootfs
 $s_mount_rw /system
 $s_mount_rw /data
 mkdir -p /tmp/
+chmod 755 /tmp/
 touch $s_info
-chmod -R 755 /tmp/
 
 if [ ! -f $s_xbin/curl ]
 then clear
@@ -30,25 +31,41 @@ sleep 1
 clear
 echo "Downloading curl binaries..."
 sleep 1
-am start -a android.intent.action.VIEW -n com.android.browser/.BrowserActivity https://goo.gl/K0C2Mq >/dev/null 2>&1
-curl="1"
+am start -a android.intent.action.VIEW -n com.android.browser/.BrowserActivity https://github.com/DeicPro/Download/releases/download/curl/curl.zip >/dev/null 2>&1
+s_curl="1"
 fi
 
 if [ "$s_curl" == 1 ]
 then while true
 do if [ -f $s_ext ]
-then clear
+then am force-stop com.android.browser
+#clear
 echo "Installing..."
 sleep 1
-am force-stop com.android.browser
 unzip -oq $s_ext -d /tmp/
-cp -f /tmp/xbin/ $s_xbin/
-cp -f /tmp/ssl/ $s_ssl/
-chmod -R 755 $s_ssl/
+break
+fi
+done
+
+while true
+do if [ -f /tmp/curl ] && [ -f /tmp/openssl/ ] && [ -f /tmp/openssl.cnf ] && [ -f /tmp/ca-bundle.crt ]
+then mkdir $s_ssl/
+mkdir $s_certs/
+cp -f /tmp/curl $s_xbin/
+cp -f /tmp/openssl $s_xbin/
+cp -f /tmp/openssl.cnf $s_ssl/
+cp -f /tmp/ca-bundle.crt $s_certs/
 chmod 755 $s_xbin/curl
 chmod 755 $s_xbin/openssl
+chmod -R 755 $s_ssl/
 rm -f $s_ext
-clear
+break
+fi
+done
+
+while true
+do if [ -f $s_xbin/curl ] && [ -f $s_xbin/openssl ] && [ -d $s_ssl/ ]
+then clear
 echo "Installed."
 sleep 1
 break
