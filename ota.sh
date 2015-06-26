26778ec43742cbd6da682df00065b8306b34199d
Deic <DeicPro@users.noreply.github.com>
2015-06-26 15:55:29 +0200

    Update ota.sh

--- ota.sh
+++ ota.sh
@@ -1,4 +1,4 @@
-#SH-OTA v1.2_alpha By Deic
+#SH-OTA v1.2_alpha By Deic & hoholee12
 
 #Edit values
 name="your_script.sh"
@@ -12,15 +12,15 @@ echo "no" > /tmp/SH-OTA.info
 exit
 }
 
-if [ "`grep $version $location/$name 2>/dev/null`" ]
-then clear
+if [ "`grep $version $location/$name 2>/dev/null`" ]; then
+clear
 echo "You have the latest version."
 sleep 1
 custom_exit
 fi
 
-while true
-do clear
+while true; do
+clear
 echo "A new version of the script was found..."
 echo
 echo "Want install it? (Y/N)"
@@ -28,11 +28,9 @@ echo
 echo -n "> "
 read install_opt
 case $install_opt in
-y|Y ) echo "yes" > /tmp/SH-OTA.info
-break;;
+y|Y ) echo "yes" > /tmp/SH-OTA.info; break;;
 n|N ) custom_exit;;
-* ) echo "Write [Y] or [N] and press enter..."
-sleep 1;;
+* ) echo "Write [Y] or [N] and press enter..."; sleep 1;;
 esac
 done
 
